1c4595447eb343d0274fe216d6fdd4ec62728185
gildas <gildas.garcia@gmail.com>
2017-05-12 15:37:09 +0200

    trying to reproduce aor problem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 import RaisedButton from 'material-ui/RaisedButton'
+import SettingsIcon from 'material-ui/svg-icons/action/settings';
 
 class App extends Component {
 
@@ -17,6 +18,13 @@ class App extends Component {
   toggleDrawer = () => this.setState({ open: !this.state.open })
 
   render() {
+      const items = [
+        { name: 'about0', path: 'about', icon: <SettingsIcon /> },
+        { name: 'about1', path: 'about', icon: <SettingsIcon /> },
+        { name: 'about2', path: 'about', icon: <SettingsIcon /> },
+        { name: 'about3', path: 'about', icon: <SettingsIcon /> },
+      ];
+
     return (
       <div>
         <AppBar
@@ -31,25 +39,36 @@ class App extends Component {
           onRequestChange={this.toggleDrawer}
           open={this.state.open}
         >
-          <AppBar title="Title" onLeftIconButtonTouchTap={this.toggleDrawer} />
           <MenuItem
             primaryText="home"
             containerElement={<Link to="/" />}
+            leftIcon={<SettingsIcon />}
             onTouchTap={() => {
               console.log('going home')
-              alert('going home!')
               this.toggleDrawer()
             }}
           />
           <MenuItem
             primaryText="about"
             containerElement={<Link to="/about" />}
+            leftIcon={<SettingsIcon />}
             onTouchTap={() => {
               console.log('about')
-              alert('going to about page!')
               this.toggleDrawer()
             }}
           />
+        {items.map(item => (
+            <MenuItem
+                key={item.name}
+                containerElement={<Link to={`/${item.path}`} />}
+                primaryText={item.name}
+                leftIcon={item.icon}
+              onTouchTap={() => {
+                console.log('about')
+                this.toggleDrawer()
+              }}
+            />
+        ))}
         </Drawer>
 
         <div style={{ textAlign: 'center' }}>
